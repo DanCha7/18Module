5352fd9dea409b3c8d3d1844cc42d841a758d431
Imac <imac@iMac-Imac.local>
2024-08-31 16:22:09 +0300

    Новое

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
-import { defineConfig, PluginOption, ViteDevServer } from 'vite';
+import { defineConfig, PluginOption} from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react() as PluginOption], // Приведение типа для плагина React
   base: "18Module",
-  server: {
-    configureServer: (server: ViteDevServer) => {
-      server.middlewares.use((req, res, next) => {
-        if (req.url?.endsWith('.ts') || req.url?.endsWith('.tsx')) {
-          res.setHeader('Content-Type', 'application/javascript');
-        }
-        next();
-      });
-    },
-  },
+
 });
 
 
